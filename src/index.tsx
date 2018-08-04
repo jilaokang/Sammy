996d41a4c63c77bf74ba90086b5065b3c08c72c3
Lurance <353650263@qq.com>
2018-08-04 15:46:16 +0800

    patch: move Home Component to home dir

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import './index.css';
 import lang from './lang';
 import registerServiceWorker from './registerServiceWorker';
 import store from "./store";
-import {Home} from "./article";
+import {Home} from "./home";
 
 ReactDOM.render(
     <IntlProvider locale={lang.locale} messages={lang.message}>
