2d33155e4a67af88c76f1c3c759b82515cc80d44
Lurance <353650263@qq.com>
2018-08-23 15:02:18 +0800

    refactor: change common redux state to mobx
    Change-Id: I32531314a53c1db9c69e20ded95f4c8e2efda3ce

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,35 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {IntlProvider} from 'react-intl';
-import {Provider} from "react-redux";
-import {HashRouter, Route, Switch} from "react-router-dom";
+import { IntlProvider } from 'react-intl';
+import { Provider } from "react-redux";
+import { Provider as MobxProvider } from 'mobx-react';
+import { HashRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 import lang from './lang';
 import registerServiceWorker from './registerServiceWorker';
 import store from "./store";
-import {Home} from "./home";
+import commonStore from './common/model';
+import { Home } from "./home";
 import ContactComponentCreator from "./contact";
-import {ArticleContainer} from "./article";
+import { ArticleContainer } from "./article";
 
 ReactDOM.render(
     <IntlProvider locale={lang.locale} messages={lang.message}>
         <Provider store={store}>
-            <HashRouter>
-                <Switch>
-                    <Route path="/" exact={true} component={Home}/>
-                    <Route path="/contact">
-                        <Switch>
-                            <Route path="/contact/qq" component={ContactComponentCreator('qq')} />
-                            <Route path="/contact/wechat" component={ContactComponentCreator('wechat')} />
-                        </Switch>
-                    </Route>
-                    <Route path="/articles" component={ArticleContainer} />
-                </Switch>
-            </HashRouter>
+            <MobxProvider commonStore={commonStore}>
+                <HashRouter>
+                    <Switch>
+                        <Route path="/" exact={true} component={Home} />
+                        <Route path="/contact">
+                            <Switch>
+                                <Route path="/contact/qq" component={ContactComponentCreator('qq')} />
+                                <Route path="/contact/wechat" component={ContactComponentCreator('wechat')} />
+                            </Switch>
+                        </Route>
+                        <Route path="/articles" component={ArticleContainer} />
+                    </Switch>
+                </HashRouter>
+            </MobxProvider>
         </Provider>
     </IntlProvider>,
     document.getElementById('root') as HTMLElement
