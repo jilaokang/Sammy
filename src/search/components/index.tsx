c5122364921b8e3e21ec0a510a52ded2c51a4e92
Lurance <353650263@qq.com>
2018-08-24 14:13:13 +0800

    feat: support search by title, tags and create time
    Change-Id: I9087c6de41338570875c3dd46e9ea128905d3e08

--- src/search/components/index.tsx
+++ src/search/components/index.tsx
@@ -1,18 +1,64 @@
 import * as React from 'react';
 
-import { WiredCard, WiredIconButton } from 'react-wired-element';
+import { WiredCard, WiredIconButton, WiredInput } from 'react-wired-element';
 
-class Search extends React.Component<{ onClose: any }> {
+import { FormattedHTMLMessage, injectIntl, InjectedIntl } from 'react-intl';
+
+import { autobind } from 'core-decorators';
+
+import { observer, inject } from 'mobx-react';
+
+import { ArticleStore } from '../../article/model';
+
+import { IArticle } from '../../@types';
+
+@inject("articleStore")
+@observer
+@autobind
+class Search extends React.Component<{ onClose: any, intl: InjectedIntl, articleStore: ArticleStore }, { search: string, searchResult: IArticle[] }> {
+    constructor(args) {
+        super(args);
+        this.state = {
+            search: '',
+            searchResult: []
+        };
+    }
+
+    public componentDidMount() {
+        document.addEventListener('change', this.changeListener);
+    }
+
+    public changeListener(ev) {
+        const value = ev.target.value;
+        const { articleStore } = this.props;
+        this.setState({ search: value, searchResult: articleStore.getArticlesBySearch(value) });
+    }
+
+    public componentWillUnmount() {
+        document.removeEventListener('change', this.changeListener);
+    }
+
     public render() {
-        const { onClose } = this.props;
+        const { onClose, intl } = this.props;
+        const { search, searchResult } = this.state;
+        const PLHD = intl.formatMessage({
+            id: 'Search.input.plhd'
+        });
+        console.log(searchResult);
         return (
             <section className="search-container animated slideInDown">
                 <WiredCard class="search">
-                    <section className="close" onClick={onClose}><WiredIconButton>clear</WiredIconButton></section>
+                    <section className="header" onClick={onClose}>
+                        <FormattedHTMLMessage id="Search.header.title"/>
+                        <WiredIconButton>clear</WiredIconButton>
+                    </section>
+                    <section className="input">
+                        <WiredInput placeholder={PLHD} name="search" value={search} />
+                    </section>
                 </WiredCard>
             </section>
         );
     }
 }
 
-export default Search;
+export default injectIntl(Search);
