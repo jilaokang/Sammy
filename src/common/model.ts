8f119b521f99769b1e13627e3414bb047e48c556
Lurance <353650263@qq.com>
2018-08-24 17:00:06 +0800

    patch: remove playground code
    Change-Id: I6cdd04bcd569e94b2d5a6044497b8faab2286ade

--- src/common/model.ts
+++ src/common/model.ts
@@ -14,7 +14,6 @@ export class CommonStore {
         if (this.showTabs.length === 0) {
             const res = await Axios.get<ShowTabs>(`${COSAPIURL}header-config.json`);
             const data = res.data;
-            data.push('search');
             this.showTabs = data;
         }
     }
