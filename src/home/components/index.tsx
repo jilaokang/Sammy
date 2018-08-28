a8e12f4fc5ac3fa3e04546cea1c2d9edf0ea7f77
Lurance <353650263@qq.com>
2018-08-28 12:28:04 +0800

    feat&refactor: home module ui change to material
    Change-Id: Ifc7e3fa43c0eec78b11274e70c8797de02b96096

--- src/home/components/index.tsx
+++ src/home/components/index.tsx
@@ -6,7 +6,15 @@ import {FormattedMessage} from 'react-intl';
 
 import lang from '../../lang';
 
-import {WiredButton, WiredCard, WiredCombo, WiredIconButton, WiredItem} from "react-wired-element";
+import Card from '@material-ui/core/Card';
+
+import Select from '@material-ui/core/Select';
+
+import MenuItem from '@material-ui/core/MenuItem';
+
+import Button from '@material-ui/core/Button';
+
+import IconButton from '@material-ui/core/IconButton';
 
 import {History} from 'history';
 
@@ -16,30 +24,21 @@ const NOW_YEAR = new Date().getFullYear();
 
 @autobind()
 class Home extends React.Component<{history: History}> {
-    public wiredComboSelectedListener: EventListenerOrEventListenerObject;
-
     constructor(args) {
         super(args);
-        this.wiredComboSelectedListener = (ev: CustomEvent) => {
-            lang.changeUserLang(ev.detail.selected);
-            location.reload();
-        };
     }
 
-    public componentDidMount() {
-        addEventListener('selected', this.wiredComboSelectedListener);
-    }
-
-    public componentWillUnmount() {
-        removeEventListener('selected', this.wiredComboSelectedListener);
+    public handlelangSelectChange(event) {
+        lang.changeUserLang(event.target.value);
+        location.reload();
     }
 
     public handleClick(v) {
-        const name = v.target.getAttribute('name');
-        switch (v.target.getAttribute('name')) {
+        const id = v.target.getAttribute('id');
+        switch (id) {
             case 'qq':
             case 'wechat':
-                this.props.history.push(`/contact/${name}`);
+                this.props.history.push(`/contact/${id}`);
                 break;
             case 'weibo':
                 window.open('https://weibo.com/sammyliang97');
@@ -57,31 +56,31 @@ class Home extends React.Component<{history: History}> {
     public render() {
         return (
             <section className='home-container animated fadeIn'>
-                <WiredCard elevation={5}>
+                <Card elevation={5}>
                     <section className='home-w-card'>
                         <section className='home-w-combo'>
-                            <WiredCombo selected={lang.locale} tabindex="0">
-                                <WiredItem value="zh" text="中文（zh-cn）"/>
-                                <WiredItem value="en" text="English（en-us）"/>
-                            </WiredCombo>
+                            <Select value={lang.locale} onChange={this.handlelangSelectChange}>
+                                <MenuItem value="zh">中文（zh-cn）</MenuItem>
+                                <MenuItem value="en">English（en-us）</MenuItem>
+                            </Select>
                         </section>
                         <section className="words">
                             <FormattedMessage id="Home.base.words"/>
                         </section>
                         <FormattedMessage id="Home.base.welcome"/>
                         <section className="w-btn">
-                            <WiredButton onClick={() => this.props.history.push('/articles')}><FormattedMessage id="Home.base.entry"/></WiredButton>
+                            <Button variant="outlined" size="large" onClick={() => this.props.history.push('/articles')}><FormattedMessage id="Home.base.entry"/></Button>
                         </section>
                         <section className="home-button-contact-list" onClick={this.handleClick}>
-                            <WiredIconButton class="iconfont icon-qq deepskyblue" name="qq" />
-                            <WiredIconButton class="iconfont icon-weibo orange" name="weibo" />
-                            <WiredIconButton class="iconfont icon-weixin green" name="wechat" />
-                            <WiredIconButton class="iconfont icon-github chocolate" name="github" />
-                            <WiredIconButton class="iconfont icon-youxiang red" name="mail" />
-                            <WiredIconButton class="iconfont icon-wo deeppink" name="about" />
+                            <IconButton><i className="iconfont icon-qq deepskyblue" id="qq" /></IconButton>
+                            <IconButton><i className="iconfont icon-weibo orange" id="weibo" /></IconButton>
+                            <IconButton><i className="iconfont icon-weixin green" id="wechat" /></IconButton>
+                            <IconButton><i className="iconfont icon-github chocolate" id="github" /></IconButton>
+                            <IconButton><i className="iconfont icon-youxiang red" id="mail" /></IconButton>
+                            <IconButton><i className="iconfont icon-wo deeppink" id="mine" /></IconButton>
                         </section>
                     </section>
-                </WiredCard>
+                </Card>
                 <br />
                 <p style={{textAlign: 'center'}}><FormattedMessage id="Home.base.footer.desc" values={{year: NOW_YEAR}}/></p>
             </section>
